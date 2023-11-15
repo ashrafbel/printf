94d1e31f4b1ea841af860fd806aa0ab6e2a61b98
ashrafbel <ashrafmus2@gmail.com>
2023-11-14 16:32:18 -0800

    printf

---
+++ Str_l.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _slen - find the lenght
+ * @s: the string
+ * Return: int
+ */
+
+int _slen(char *s)
+{
+        int K = 0;
+while (s[K] != '\0')
+{
+K++;
+}
+return (K);
+}
--- Str_p.c
+++ Str_p.c
@@ -1,21 +1,5 @@
 #include "main.h"
 /**
- * _slen - find the lenght
- * @s: the string
- * Return: int
- */
-
-int _slen(char *s)
-{
-	int K = 0;
-while (s[K] != '\0')
-{
-K++;
-}
-return (K);
-}
-
-/**
  * p_string - print a string.
  * @val: argument
  * Return: length of  string
